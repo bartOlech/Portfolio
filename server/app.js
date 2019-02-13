bdb5444285dc8f81081d034ff76941af791f3d27
Olech Bartlomiej <olech.bartlomiej@gmail.com>
2019-02-13 18:55:58 +0100

    add a nodemailer to the sever

--- server/app.js
+++ server/app.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const router = require('./routes/index')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+
+const corsOption = {
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+};
+
+app.use(cors(corsOption));
+
 app.use('/api', router)
 
 module.exports = app;
--- server/controllers/mailController.js
+++ server/controllers/mailController.js
@@ -1,4 +1,45 @@
+const nodemailer = require('nodemailer')
+const conf = require('../config/config')
 
 exports.mail = (req, res) => {
-    console.log('work :)')
+    const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.msg}</p>
+  `;
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: conf.config.login, // generated ethereal user
+            pass: conf.config.pass  // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    let mailOptions = {
+        from: '"Nodemailer Contact" olech.bartlomiej@gmail.com', // sender address
+        to: conf.config.login, // list of receivers
+        subject: 'Node Contact Request', // Subject line
+        text: 'Hello world?', // plain text body
+        html: output // html body
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    });
 }
